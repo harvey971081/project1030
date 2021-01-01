d008e24441efe1fcfe28c3ac54e80e14eee5422f
Harvey <harvey971081@icloud.com>
2021-01-01 18:20:08 +0800

    new js.

---
+++ app.js
@@ -0,0 +1,13 @@
+let log = console.log;
+let myInt = 123;
+let myStrInt = "456";
+let address = "456 Main Ave.";
+let myFloat = 123.456;
+let myStrFloat = "345.567.789.345";
+let cost = "12.99 per box";
+
+log(parseInt(address));
+log(parseFloat(address));
+log(parseInt(myStrFloat));
+log(parseInt(cost));
+
